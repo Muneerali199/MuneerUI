25b1a295deb71ac6ff02713234a21eff3cf2e0ac
Muneerali199 <alimuneerali245@gmail.com>
2025-10-20 00:37:18 +0530

    update: src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './styles.css';
+import './muneerui-theme.css';
+
+import { Provider } from '@/components/setup/provider';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+
+ReactDOM.createRoot(document.createElement('div')).render(
+  // eslint-disable-next-line react/no-children-prop
+  <SyntaxHighlighter language="" children={''} />
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider>
+    <App />
+  </Provider>
+);
