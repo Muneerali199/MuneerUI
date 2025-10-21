de67beffd014504ab6f03ab30dfffeac9ff0a70c
Muneerali199 <alimuneerali245@gmail.com>
2025-10-22 01:40:50 +0530

    update: src/components/common/Misc/Announcement.jsx

--- src/components/common/Misc/Announcement.jsx
+++ src/components/common/Misc/Announcement.jsx
@@ -104,14 +104,14 @@ const Announcement = () => {
           const file = new File([blob], 'vue-bits.jpg', { type: 'image/jpeg' });
 
           await navigator.share({
-            title: 'Vue Bits - Official Vue Port of React Bits',
+            title: 'Vue Bits - Official Vue Port of MuneerUi',
             text: shareText,
             files: [file]
           });
         } catch (error) {
           try {
             await navigator.share({
-              title: 'Vue Bits - Official Vue Port of React Bits',
+              title: 'Vue Bits - Official Vue Port of MuneerUi',
               text: shareText,
               url: window.location.origin
             });
