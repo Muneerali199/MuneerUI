9ec2db4254d11b962495a070bb820d38142f8d7d
Muneerali199 <alimuneerali245@gmail.com>
2025-10-22 01:40:58 +0530

    update: src/constants/code/TextAnimations/fallingTextCode.js

--- src/constants/code/TextAnimations/fallingTextCode.js
+++ src/constants/code/TextAnimations/fallingTextCode.js
@@ -9,8 +9,8 @@ export const fallingText = {
   usage: `import FallingText from './FallingText';
   
 <FallingText
-  text={\`React Bits is a library of animated and interactive React components designed to streamline UI development and simplify your workflow.\`}
-  highlightWords={["React", "Bits", "animated", "components", "simplify"]}
+  text={\`MuneerUI is a library of animated and interactive React components designed to streamline UI development and simplify your workflow.\`}
+  highlightWords={["MuneerUI", "animated", "components", "simplify"]}
   highlightClass="highlighted"
   trigger="hover"
   backgroundColor="transparent"
