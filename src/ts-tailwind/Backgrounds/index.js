54c30efdd9753c7cda81347ec1d0124f833d0d3c
Muneerali199 <alimuneerali245@gmail.com>
2025-10-22 01:41:19 +0530

    update: src/ts-tailwind/Backgrounds/index.js

---
+++ src/ts-tailwind/Backgrounds/index.js
@@ -0,0 +1,10 @@
+// New Background Components - TailwindCSS Versions
+export { default as CosmicDustTailwind } from './CosmicDust/CosmicDustTailwind';
+export { default as AuroraWaves } from './AuroraWaves/AuroraWaves';
+export { default as NeuralNetwork } from './NeuralNetwork/NeuralNetwork';
+export { default as ParticleField } from './ParticleField/ParticleField';
+export { default as GlowingGrid } from './GlowingGrid/GlowingGrid';
+export { default as MatrixRain } from './MatrixRain/MatrixRain';
+export { default as FloatingBubbles } from './FloatingBubbles/FloatingBubbles';
+export { default as DigitalWaves } from './DigitalWaves/DigitalWaves';
+export { default as StarField } from './StarField/StarField';
