91f056b8c2e8899a5ab50e7f682ef18025ed9d3c
Muneerali199 <alimuneerali245@gmail.com>
2025-10-22 01:40:54 +0530

    update: src/components/landing/StartBuilding/StartBuilding.jsx

--- src/components/landing/StartBuilding/StartBuilding.jsx
+++ src/components/landing/StartBuilding/StartBuilding.jsx
@@ -9,7 +9,7 @@ const StartBuilding = () => {
           <h2 className="start-building-title">Start Exploring</h2>
           <p className="start-building-subtitle">Animations, Components, Backgrounds - One Click Away</p>
 
-          <Link to="/text-animations/split-text" className="start-building-button">
+          <Link to="/animations/blob-cursor" className="start-building-button">
             Browse Components
           </Link>
         </div>
